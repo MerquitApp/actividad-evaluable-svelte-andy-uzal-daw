326b82712b3ed4a62b7ba9c0890dc6c7c3bb63c8
Andy Uzal <andyuzal@gmail.com>
2024-11-21 10:41:15 +0100

    feat: Agregando Main con imagen

--- src/lib/components/Main/Main.svelte
+++ src/lib/components/Main/Main.svelte
@@ -1,10 +1,16 @@
 <script>
-    
+    import mainImagen from '../../../assets/main_imagen.avif';
 </script>
 
-<main>
-    <img
-    class="" 
-    src="" 
-    alt="">
+<main class="h-3/5 flex justify-center">
+    <div class="bg-gray-200 w-1/5">
+    </div>
+    <div>
+        <img
+        class="w-max"
+        src={mainImagen}
+        alt="">
+    </div>
+    <div class="bg-gray-200 w-1/5">
+    </div>
 </main>
--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -2,11 +2,13 @@
     import Article from "../lib/components/Article/Article.svelte";
     import Footer from "../lib/components/Footer/Footer.svelte";
     import Header from "../lib/components/Header/Header.svelte";   
+    import Main from "../lib/components/Main/Main.svelte";
     import SectionProducts from "../lib/components/Section/SectionProducts.svelte";
 </script>
 
 <div>
     <Header />
+    <Main />
     <Article 
     tituloArticle="Zapatillas de deporte" 
     subTituloArticle="¡Al mejor precio!"
